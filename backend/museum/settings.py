c47ad2c021e0b97012893f03db6b9d7af5e07396
AlistairBishop06 <alistairbishop@gmx.co.uk>
2026-01-19 19:53:08 +0000

    django server up

--- backend/museum/settings.py
+++ backend/museum/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "exhibits",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
