e99f2f0fdfdcaab86dd4420888756e756db7cfd9
AlistairBishop06 <alistairbishop@gmx.co.uk>
2026-03-17 14:29:29 +0000

    added splash image

--- backend/museum/settings.py
+++ backend/museum/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Media files (User uploaded content)
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
